7e3e2a24d4fa69d031448cacaa5a77af323fd7a7
Daniel Brandt <daniel.gordon.brandt@gmail.com>
2021-06-10 18:20:15 +0200

    creation of react app + basic hello

---
+++ accenture-sigle-page-magic/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+    // want a header or navbar here, fancy scroller?
+
+    <div>
+      <h1>So it Begins</h1>
+      <p>Inro text to Accenture Here</p>
+    </div>
+
+
+  );
+}
+
+export default App;
