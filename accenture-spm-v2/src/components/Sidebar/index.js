3026167a66b8d77c435671c72ba722c76d6806f4
Daniel Brandt <daniel.gordon.brandt@gmail.com>
2021-06-13 19:44:42 +0200

    info section styled

--- accenture-spm-v2/src/components/Sidebar/index.js
+++ accenture-spm-v2/src/components/Sidebar/index.js
@@ -10,7 +10,7 @@ import { SidebarContainer,
        } from './SidebarElements';
 
 
-const Sidebar = ({isOpen, toggle}) => {
+const Sidebar = ({ isOpen, toggle }) => {
   return (
     <SidebarContainer isOpen={isOpen} onClick={toggle}>
       <Icon onClick={toggle}>
