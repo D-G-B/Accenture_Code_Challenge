3ef826fdde66e0d71046b0650eb1f0c79f4d9d7b
Daniel Brandt <daniel.gordon.brandt@gmail.com>
2021-06-13 18:08:03 +0200

    added hero section and button with styling. updated font

--- accenture-spm-v2/src/components/HeroSection/index.js
+++ accenture-spm-v2/src/components/HeroSection/index.js
@@ -1,12 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Video from '../../videos/Network-Dark.mp4';
+import { Button } from '../ButtonElement';
+import { HeroContainer, 
+         HeroBg,
+         VideoBg,
+         HeroContent,
+         HeroH1,
+         HeroP,
+         HeroBtnWrapper,
+         ArrowForward,
+         ArrowRight
+       } from './HeroElements';
 
 const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover)
+  };
+
   return (
     <HeroContainer>
       <HeroBg>
         <VideoBg autoPlay loop muted src={Video} type='video/Network-Dark.mp4' />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Our Purpose:</HeroH1>
+        <HeroP>
+          Delivering on the promise of technology and human ingenuity, we embrace the power of change to create 360° value for our clients, people and communities.
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button to='signup' onMouseEnter={onHover} onMouseLeave={onHover}
+          primary='true'
+          dark='true'
+          >
+            Get started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
     </HeroContainer>
-  )
+  );
 };
 
-export default HeroSection
+export default HeroSection;
