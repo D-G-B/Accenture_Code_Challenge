0361ac4092bd4f5bbf18a74a5fce5e957411b97d
Daniel Brandt <daniel.gordon.brandt@gmail.com>
2021-06-13 16:58:57 +0200

    cleaned up some files. navbar and sidebard done. started adding hero sec. added images

---
+++ accenture-spm-v2/src/components/HeroSection/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <HeroContainer>
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type='video/Network-Dark.mp4' />
+    </HeroContainer>
+  )
+};
+
+export default HeroSection
